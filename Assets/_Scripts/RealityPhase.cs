367c59839adecb76f95410f83d380fa84a12734f
Dylan <guidry.dylan.95@gmail.com>
2021-03-05 22:36:56 -0500

    Eric is bad at everything

---
+++ Assets/_Scripts/RealityPhase.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealityPhase : MonoBehaviour
+{
+    public List<RenderLayers> Layers;
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            ToggleLayer(new System.Object[] { Camera.main, Layers[0] });
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            ToggleLayer(new System.Object[] { Camera.main, Layers[1] });
+        }
+    }
+
+    public void ToggleLayer(System.Object[] args)
+    {
+        if (args[0].GetType() != typeof(Camera))
+        {
+            Debug.LogError("First argument must be a camera");
+            return;
+        }
+
+        foreach (var layer in Layers)
+        {
+            if (layer == args[1] as RenderLayers)
+            {
+                layer.EnableLayer(args[0] as Camera);
+            }
+        }        
+    }
+}
+
+[System.Serializable]
+public class RenderLayers
+{
+    public string Name;
+    public bool IsRendering;
+    [SerializeField]
+    public UnityEngine.Events.UnityEvent LayerIsRendering;
+    public UnityEngine.Events.UnityEvent LayerNotRendering;
+
+    public void EnableLayer(Camera cam)
+    {
+        cam.cullingMask = cam.cullingMask ^ (1 << LayerMask.NameToLayer(Name));
+        IsRendering = !IsRendering;
+        if (IsRendering)
+            LayerIsRendering.Invoke();
+        else
+            LayerNotRendering.Invoke();
+    }
+}
---
+++ Assets/_Scripts/RealityPhysicsCheck.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealityPhysicsCheck : MonoBehaviour
+{
+    public int PreviousLayer;
+
+    private void Awake()
+    {
+        PreviousLayer = gameObject.layer;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("HIT");
+        if (collision.gameObject.layer != this.gameObject.layer)
+            Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), this.gameObject.GetComponent<Collider>(), true);
+        else
+            Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), this.gameObject.GetComponent<Collider>(), false);
+    }
+
+    private void Update()
+    {        
+        UpdateRenderLayer();
+    }
+
+    void UpdateRenderLayer()
+    {
+        if(gameObject.layer != PreviousLayer)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up);
+        }
+        PreviousLayer = gameObject.layer;
+    }
+}
