85fa9d906e25b0800da38abf79703c755ab68351
Dylan <guidry.dylan.95@gmail.com>
2021-03-06 01:39:41 -0500

    IDK

---
+++ Assets/_Scripts/SpriteOverlap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteOverlap : MonoBehaviour
+{    
+    public List<GameObject> Overlapped;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Overlapped.Add(other.gameObject);
+    }    
+
+    private void OnTriggerStay(Collider2D other)
+    {
+        if (Overlapped.Contains(other.gameObject))
+            return;        
+        Overlapped.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider2D other)
+    {
+        if (Overlapped.Contains(other.gameObject))            
+            Overlapped.Remove(other.gameObject);
+    }
+}
