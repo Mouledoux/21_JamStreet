6f66ea648079fbd5a64d3afc33cc05ca59d967b9
Eric <EricMouledoux@Gmail.com>
2021-03-06 17:39:55 -0600

    ui

---
+++ Assets/_Scripts/UiUtils.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiUtils : MonoBehaviour
+{
+    public void CloseApplication()
+    {
+        Application.Quit();
+    }
+}
