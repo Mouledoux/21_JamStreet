e8826696ab5f12ed438498616ba9491c26fc884e
Dylan <guidry.dylan.95@gmail.com>
2021-03-06 16:07:34 -0500

    idk

---
+++ Assets/_Scripts/PickupBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupBehaviour : MonoBehaviour
+{
+    public Pickupable ActivePickup;
+    public Transform GrabLocation;
+
+    public void PickupPrompt(GameObject obj)
+    {
+        if(obj.GetComponent<Pickupable>())
+        {
+            obj.GetComponent<Pickupable>().DisplayPrompt(true);
+        }
+    }
+
+    public void StopPickupPrompt(GameObject obj)
+    {
+        if (obj.GetComponent<Pickupable>())
+        {
+            obj.GetComponent<Pickupable>().DisplayPrompt(true);
+        }
+    }
+}
---
+++ Assets/_Scripts/Pickupable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupable : MonoBehaviour
+{
+    private bool pickable;
+    public GameObject Prompt;
+        
+    public void DisplayPrompt(bool status)
+    {
+        Prompt.SetActive(status);
+    }
+}
